ceb7489ed41266147ae108147f639fa8d9b8bdc3
shruti-doon <shruti23cc12@gmail.com>
2024-06-05 06:35:39 +0530

    update

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -8,8 +8,8 @@ import * as flipflopjs from "./flipflop.js";
 let num_wires = 0;
 
 document.getScroll = function () {
-    if (window.pageYOffset != undefined) {
-        return [pageXOffset, pageYOffset];
+    if (window.scrollY != undefined) {
+        return [scrollX, scrollY];
     } else {
         let sx, sy, d = document,
             r = d.documentElement,
@@ -25,7 +25,7 @@ export const jsPlumbInstance = jsPlumbBrowserUI.newInstance({
     maxConnections: -1,
     endpoint: {
         type: "Dot",
-        options: { radius: 7 },
+        options: { radius: 6 },
     },
     dragOptions: {
         containment: "parentEnclosed",
@@ -1498,7 +1498,20 @@ export function refreshWorkingArea() {
     gatejs.clearGates();
     flipflopjs.clearFlipFlops();
 }
+refresh.addEventListener("click",function()
+{
+    jsPlumbInstance.reset();
+    window.numComponents = 0;
+    window.firstSimulation = true;
+    gatejs.clearGates();
+    flipflopjs.clearFlipFlops();
+     if(window.currentTab=="task1") initTFlipFlop();
+    else if(window.currentTab=="task2") initDFlipFlop();
+    // else if(window.currentTab=="task3") initTFlipFlop();
+    // else if(window.currentTab=="task4") initTFlipFlop();
+    else if(window.currentTab=="task3") initFreqDivider() 
 
+})
 window.currentTab = "task1";
 connectJKFF();
 refreshWorkingArea();
